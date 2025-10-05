1b453f957198acad9de284b5f6fecb682ecac6fb
Hadi Raza <hadiraza.edu@gmail.com>
2025-10-05 16:01:55 +0500

    bubble sort

---
+++ bubbleSort.js
@@ -0,0 +1,13 @@
+function bubbleSort(arr) {
+    for(let i=0; i<arr.length-1; i++) {
+        for(let j=0; j<arr.length-i-1; j++) {
+            if(arr[j] > arr[j+1]) {
+                let temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+console.log(bubbleSort([9, 4, 1, 2, 6, 8, 0, 2]));
