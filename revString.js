779c182df54c59db85ff5ee2c5e3faa4d2a513be
Hadi Raza <hadiraza.edu@gmail.com>
2025-10-15 22:12:53 +0500

    reverse a string or Palindrome

---
+++ revString.js
@@ -0,0 +1,8 @@
+function revStr(str) {
+    let reversedStr = ''
+    for(let i=str.length-1; i>=0; i--) {
+        reversedStr += str[i]
+    }
+    return reversedStr;
+}
+console.log(revStr('racecar'));
