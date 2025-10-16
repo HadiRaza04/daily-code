6bed88325086328a531e3ac28a856575a67b30b3
Hadi Raza <hadiraza.edu@gmail.com>
2025-10-16 14:24:09 +0500

    edit divisible-sum-pairs

--- hacker_rank/divisible-sum-pairs.js
+++ hacker_rank/divisible-sum-pairs.js
@@ -4,16 +4,13 @@
 function divisibleSumPairs(n, k, ar) {
     let count = 0
     for(let i=0; i<n; i++) {
-        for(let j=0; j<n; j++) {
-            if(ar[i]<ar[j]) {
-                let sum = ar[i] + ar[j]
-                if(sum % k == 0) {
-                    count++
-                }
+        for(let j=i+1; j<n; j++) {
+            let sum = ar[i] + ar[j]
+            if(sum % k === 0) {
+                count++
             }
         }
     }
     return count;
-
 }
 console.log(divisibleSumPairs(6, 3, [1, 3, 2, 6, 1, 2]));  // 5
