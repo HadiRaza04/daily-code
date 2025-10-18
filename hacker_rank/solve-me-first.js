d48cddd03b722d03c21f4bae72966e93e11975b6
Hadi Raza <hadiraza.edu@gmail.com>
2025-10-18 11:07:34 +0500

    solve-me-first

---
+++ hacker_rank/solve-me-first.js
@@ -0,0 +1,19 @@
+// solve-me-first
+// https://www.hackerrank.com/challenges/solve-me-first/problem?isFullScreen=true
+
+// DESCRIPTION
+// Complete the function solveMeFirst to compute the sum of two integers.
+
+// Example
+// a = 7
+// b = 3
+
+// Return 10.
+
+// Code
+function solveMeFirst(a, b) {
+  return a+b;
+}
+console.log(solveMeFirst(2, 7));  // 9
+console.log(solveMeFirst(34, 7));  // 41
+console.log(solveMeFirst(8, 10));  // 18
