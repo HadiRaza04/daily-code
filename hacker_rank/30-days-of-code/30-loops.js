7f0e8acf8ffe409abee77df92e4aafe5d74e636c
Hadi Raza <hadiraza.edu@gmail.com>
2025-11-07 14:34:00 +0500

    hackerrank30daysOfCode-loops

---
+++ hacker_rank/30-days-of-code/30-loops.js
@@ -0,0 +1,9 @@
+// 30-loops
+// https://www.hackerrank.com/challenges/30-loops/problem?isFullScreen=true
+
+function main(n) {
+    for(let i=1; i<11; i++) {
+        console.log(n + " x " + i + " = " + 2*i);
+    }
+}
+main(2);
