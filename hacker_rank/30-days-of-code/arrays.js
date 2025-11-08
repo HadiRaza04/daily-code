4c1c85c078f99b557e718f645ae23d74186b9124
Hadi Raza <hadiraza.edu@gmail.com>
2025-11-08 19:05:16 +0500

    hackerrank30daysOfCode-arrays

---
+++ hacker_rank/30-days-of-code/arrays.js
@@ -0,0 +1,8 @@
+// arrays
+// https://www.hackerrank.com/challenges/30-arrays/problem?isFullScreen=true
+
+function main(arr) {
+    let a = arr.reverse();
+    console.log(...a);
+}
+main([3, 2, 1]);
