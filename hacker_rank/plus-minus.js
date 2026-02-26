5c676c43413a321cfd4456b0a9e41e15261c2e85
Hadi Raza <hadiraza.edu@gmail.com>
2026-02-26 10:45:16 +0500

    hackerrank-plus-minus-typescript

--- hacker_rank/plus-minus.js
+++ hacker_rank/plus-minus.js
@@ -18,3 +18,25 @@ function plusMinus(arr) {
 }
 plusMinus([-2, -1, 0, 0, 1, 2]); // 0.33 0.33 0.33
 plusMinus([1, 1, 0, -1, -1]); // 0.4 0.4 0.2
+
+
+// // TypeScript version:
+// function plusMinus(arr: number[]): void {
+//     // Write your code here
+//     let pos: number=0;
+//     let neg: number=0;
+//     let zeros: number=0;
+//     let len: number = arr.length;
+//     for(let i of arr) {
+//         if(i > 0) {
+//             pos += 1;
+//         } else if(i < 0) {
+//             neg += 1;
+//         } else {
+//             zeros += 1;
+//         }
+//     }
+//     console.log((pos/len).toFixed(6));
+//     console.log((neg/len).toFixed(6));
+//     console.log((zeros/len).toFixed(6));
+// }
