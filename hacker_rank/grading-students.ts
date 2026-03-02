981dceca9727b89bcd6cc7acad91dc6d179040c0
Hadi Raza <hadiraza.edu@gmail.com>
2026-03-02 14:20:08 +0500

    hackerrank-ts-grade-student

---
+++ hacker_rank/grading-students.ts
@@ -0,0 +1,17 @@
+// Grading Students
+// https://www.hackerrank.com/challenges/grading/problem?isFullScreen=true
+
+function gradingStudents(grades: number[]): number[] {
+    for(let i:number =0; i<grades.length; i++) {
+        if(grades[i] < 38) {
+            continue;
+        }
+        else {
+            if(((Math.ceil(grades[i] / 5) * 5) - grades[i]) < 3 ){
+                grades[i] = (Math.ceil(grades[i] / 5) * 5)
+            }
+        }
+    }
+    return grades;
+}
+console.log(gradingStudents([12, 45, 23, 76, 90, 43, 98, 36])); // [12, 45, 23, 76, 90, 45, 100, 36]
