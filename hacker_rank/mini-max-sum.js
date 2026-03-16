a9abd8dbb116ff7cd182604352ffe96111a06352
Hadi Raza <hadiraza.edu@gmail.com>
2026-03-16 12:30:43 +0500

    typescript version

--- hacker_rank/mini-max-sum.js
+++ hacker_rank/mini-max-sum.js
@@ -10,3 +10,15 @@ function miniMaxSum(arr) {
     console.log(sum-max, sum-min);
 }
 miniMaxSum([1, 3, 5, 7, 9]);
+
+// TYPESCRIPT
+// function miniMaxSum(arr: number[]): void {
+//     // Write your code here
+//     arr = arr.sort();
+//     let sum:number = 0;
+//     for(let i of arr) {
+//         sum += i;
+//     }
+//     console.log(sum-arr[arr.length-1], sum-arr[0]);
+
+// }
